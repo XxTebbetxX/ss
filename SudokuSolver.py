89815d95f8edb325fe1aa9cb2f94004962012434
Tatianna Benjamin <Tatianna.Benjamin.20@cnu.edu>
2021-09-19 16:01:58 -0400

    Organized code some and developed algorithm for guesser

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,51 +1,24 @@
 
+import puzzles as p
 
 
-
+# Get the given values from the user or an existing board
+# and fill them into the sudoku board with appropriate data types
 def setUpBoard(op, given1, given2, given3):
-    # Easy1
-    #given1 = '036100052100240000405000080'
-    #given2 = '070480265200000008658027010'
-    #given3 = '060000809000093006740002530'
 
-    #Easy2
-    #given1 = '080010095900028100007000034'
-    #given2 = '030106489090000010561904020'
-    #given3 = '210000300009230001370060040'
-
-    #Medium1
-    #given1 = '500300061030580097000900043'
-    #given2 = '000005000308000406000200000'
-    #given3 = '710002000890014030240003008'
-
-    #Hard1
-    #given1 = '800005010005080004300107000'
-    #given2 = '200060000057020340000070002'
-    #given3 = '000809007500040200040700003'
-
-    #Evil
-    #given1 = '000009800000450060906008000'
-    #given2 = '800060005010805070600090002'
-    #given3 = '000900704030047000002500000'
-
-    #Evil2
-    #given1 = '000000304802100000060094020'
-    #given2 = '050047900000000000007650040'
-    #given3 = '010520070000009602504000000'
-
-    #Evil3
-    given1 = '005320006200894000000000030'
-    given2 = '400000800027000540008000009'
-    given3 = '060000000000716008300058100'
-
-
+    # Convert the given strings
     given1 = userInput(given1)
     given2 = userInput(given2)
     given3 = userInput(given3)
 
+    # Build the board:
+    # Each box will have an array storing it's values
+    # [value, unavailable numbers, available numbers, i]
     for i in range(0,81):
         op[i] = [' ',[], [1,2,3,4,5,6,7,8,9],i]
 
+
+    # Fill in the given values in its respective box
     for i in range(0,27):
         op[i][0] = given1[i]
 
@@ -64,6 +37,7 @@ def setUpBoard(op, given1, given2, given3):
             op[i][2] = []
 
 
+# Display the sudoku board
 def display(op):
     print("\n\n\n\t\t\t {} | {} | {}    {} | {} | {}    {} | {} | {} ".format(op[0][0],op[1][0],op[2][0],op[3][0],op[4][0],op[5][0],op[6][0],op[7][0],op[8][0]))
     print("\t\t\t___________  ___________  ___________")
@@ -83,6 +57,13 @@ def display(op):
     print("\t\t\t___________  ___________  ___________")
     print("\t\t\t {} | {} | {}    {} | {} | {}    {} | {} | {} \n\n\n".format(op[72][0],op[73][0],op[74][0],op[75][0],op[76][0],op[77][0],op[78][0],op[79][0],op[80][0]))
 
+# Go through each slot of the sudoku puzzle and update the options
+# each slot has for the values available and unavailable to it.
+# If the value in the slot is blank, check the the values in the
+# other boxes, rows, or columns (depending on what was sent over) and put
+# whatever values found in the slot's unavaible list and remove it from
+# its available list. If the slot is not blank, make both its available
+# and unavailable lists empty
 def updateOptions(items):
     for item in items.values():
         for slot in item:
@@ -99,7 +80,9 @@ def updateOptions(items):
                 slot[2] = []
 
 def chosenOnes(items):
-    #Check if only 2 numbers can go in 2 boxes and eliminate from other boxes in that component other than those 2
+    #Check if only 2 numbers can go in 2 slots and eliminate the option from other slots in that box, row, or
+    # column (depending on what was passed) other than those 2. Basically, add those 2 values to the unavailable 
+    # list for all slots in that category but those 2.
     for item in items.values():
         chosen2 = []
         for box in item:   
@@ -127,9 +110,23 @@ def chosenOnes(items):
                         if chosen2[i][2][1] in box[2]:
                             box[1].append(chosen2[i][2][1])
                             box[2].remove(chosen2[i][2][1])
+    
+
     #Guesser
+    # Here's what I'm thinking: I can find a category with the least amount of empty slots and guess one of them 
+    # to be right. Then i can follow that path with the other solving methods and every time i run into a roadblock,
+    # I guess from the category with the fewer slots. In the end, if the puzzle cannot be solved, then i need to backtrack guesses.
+    # Problems: How to keep track of the board when backtracking? because we don't just have to backtrack guesses,
+    # we also have to undo other moves made bc of the guesses... maybe keep versions of the board before each guess?
+
+    # Current algorithm to implement: If I find a row with 2 options, I guess which option goes where.
+    # I continue to use the solving methods until I reach a road block. Then make another guess from 3 options
+    # Now I've made all the moves I can and the puzzle isn't solved. I need to go back to the 3 option version and try another combo of the 3
+    # I've made all moves I can and it still doesn't work. I try the final combo of the 3
+    # Again, it doesn't work, I back track and go back to the first guess. Ideally, if i've made every move I can possibly make with the 
+    #   first option and none of them panned out, the other option has to be the correct option. I need to then store it in the correct list
 def guesser(op, boxes, rows, columns, correct): 
-    guess = search(rows)
+    guess = search(rows, 2)
     print(guess)
     if guess != 0:
         for i in range(0,81):
@@ -137,7 +134,7 @@ def guesser(op, boxes, rows, columns, correct):
                 op[i][0] = op[i][2][0]
                 return op[i][3]
 
- 
+# Reset board to what it was before the incorrect guess was made
 def incorrect(correct, guess):
     for i in range(0,81):
         if op[i][3] in correct:
@@ -146,7 +143,7 @@ def incorrect(correct, guess):
 
 
 
-def search(items):
+def search(items, num):
     empty = 0
     temp = []
     for item in items.values():
@@ -154,13 +151,14 @@ def search(items):
             if square[0] == " ":
                 empty += 1
                 temp.append(square)
-        if empty == 2:
+        if empty == num:
             return temp
         else:
             empty = 0
             temp = []
     return 0
 
+# Store the values of the board to keep track of progress being made
 def progressTracker(op):
     progress = []
     for item in range(0,81):
@@ -169,6 +167,9 @@ def progressTracker(op):
     return progress
 
 
+# Update the options for boxes, rows, and columns and after doing so,
+# if that leaves any boxes, rows, or columns with only 2 options for 2 of their slots,
+# remove those two options from any other slots in that category
 def uODriver(boxes, rows, columns):
     updateOptions(boxes)
     updateOptions(rows)
@@ -177,6 +178,9 @@ def uODriver(boxes, rows, columns):
     chosenOnes(rows)
     chosenOnes(columns)
     
+# Goes through all of the slots in the puzzle and if there is a slot
+# that only has one available option, it assigns it that value and clears
+# its available and unavailable lists
 def lastManStanding(op):
     for i in range(0,81):
         if op[i][0] == " ":
@@ -186,7 +190,7 @@ def lastManStanding(op):
                 op[i][2] = []
     uODriver(boxes, rows, columns)
 
-#Check to see if there i only one number available to go in a row, box, or column
+#Check to see if there is only one number available to go in a row, box, or column
 def elimination(items):
     for x in items.values():
         pos = []
@@ -207,34 +211,44 @@ def elimination(items):
                     if item == temp:
                         item[0] = i
     
-
+# Check each row, box, and column to see if there's only one option,
+# Then update the options in case another value was found
 def eliminationDriver(boxes, rows, columns):
     elimination(boxes)
     elimination(rows)
     elimination(columns)
     uODriver(boxes, rows, columns)
 
+
+# Go through the string of values and convert every number 1-9 into a integer
+# and every 0 into a blank space: " "
 def userInput(given):
     x1 = []
     for i in given:
-        x1.append(int(i))
-    for i in range(0,27):
-        if x1[i] == 0:
-            x1[i] = " "
+        if i == '0':
+            x1.append(" ")
+        else:
+            x1.append(int(i))
     return x1
 
+# Check to see if the puzzle is completed by making sure there are no blank slots
 def completed(op):
     for i in range(0,81):
         if op[i][0] == " ":
             return False
     return True
 
+# After trying all of the solving methods, if no other values have been found,
+# then we've reached a roadblock and may need to starting making guesses
 def roadblock(past, present):
     if past == present:
         return True
     else:
         return False
 
+
+# Count how many slots have no more options. If all of them are out of options,
+# return True. Otherwise, return False.
 def noMoves(op):
     counter = 0
     for i in range(0, 81):
@@ -259,9 +273,9 @@ def solve(op, boxes, rows, columns, correct):
         for i in range(0,81):
             if op[i] != " ":
                 correct.append(op[i][3])
-        while(noMoves(op) == False):
-            g = guesser(op, boxes, rows, columns, correct)
-            guesses
+        # while(noMoves(op) == False):
+        #     g = guesser(op, boxes, rows, columns, correct)
+        #     guesses
 
 
 ###################################################################################################
@@ -269,13 +283,45 @@ def solve(op, boxes, rows, columns, correct):
 ###################################################################################################
 
 op = [0] * 81
-print("Create your puzzle by entering the numbers from left to right, row by row. Enter a 0 for blanks.")
-given1 = (input("Enter the numbers for the first 3 rows here:\n"))
-given2 = (input("Enter the numbers for the middle 3 rows here:\n"))
-given3 = (input("Enter the numbers for the last 3 rows here:\n"))
+menu_option = int(input("Welcome to the Sudoku Solver!\nMenu:\n\t(1)Use an existing puzzle\n\t(2)Create you own\n"))
+correct_input = False
+while(not correct_input):
+    if menu_option == 1:
+        correct_input = True
+        valid_entry = False
+        while(not valid_entry):
+            level = int(input("Choose level of difficulty:\n\t(1) Easy\n\t(2) Medium\n\t(3) Hard\n\t(4) Evil\n"))
+            if level == 1:
+                valid_entry = True
+                given1, given2, given3 = p.easy()
+            elif level == 2:
+                valid_entry = True
+                given1, given2, given3 = p.medium()
+            elif level == 3:
+                valid_entry = True
+                given1, given2, given3 = p.hard()
+            elif level == 4:
+                valid_entry = True
+                given1, given2, given3 = p.evil()
+            else:
+                level = int(input("Invalid entry. Please try again."))
+
+
+        
+    elif menu_option == 2:
+        correct_input = True
+        print("Create your puzzle by entering the numbers from left to right, row by row. Enter a 0 for blanks.")
+        given1 = (input("Enter the numbers for the first 3 rows here:\n"))
+        given2 = (input("Enter the numbers for the middle 3 rows here:\n"))
+        given3 = (input("Enter the numbers for the last 3 rows here:\n"))
+    else:
+        menu_option  = int(input("Invalid entry. Please try again."))
+
+
 correct = []
 setUpBoard(op, given1, given2, given3)
 
+# Format boxes, columns, and rows to be able to check only 1 of each number 1-9 is in each category
 boxes = {'box1' : [op[0], op[1], op[2], op[9], op[10], op[11], op[18], op[19], op[20]],
              'box2' : [op[3], op[4], op[5], op[12], op[13], op[14], op[21], op[22], op[23]],
              'box3' : [op[6], op[7], op[8], op[15], op[16], op[17], op[24], op[25], op[26]],
